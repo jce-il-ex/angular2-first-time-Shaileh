32689224abec6efcf4f76d9865ff2912f4ffed02
Shai Lehmann <Shai Lehmann>
2017-03-07 10:52:59 +0200

    loop and array

--- my-app/src/app/list/list.component.ts
+++ my-app/src/app/list/list.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+   myList=[{
+    id:1
+  },
+  {id:2},
+  {id:3},
+  {id:4}
+  ];
   constructor() { }
 
   ngOnInit() {
